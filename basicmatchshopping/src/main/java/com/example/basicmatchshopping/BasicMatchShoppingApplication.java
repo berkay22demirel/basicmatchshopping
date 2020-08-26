882dada31fcd680bd96f6a74cb700fd45d6cb604
berkaydemirel <berkay_demirel22@hotmail.com>
2020-08-26 23:54:03 +0300

    initial commit

---
+++ basicmatchshopping/src/main/java/com/example/basicmatchshopping/BasicMatchShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicmatchshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicMatchShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicMatchShoppingApplication.class, args);
+	}
+
+}
